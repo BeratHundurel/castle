599b024e13de34166b8e37fc660318e38454514d
Berat Hündürel <berat.hundurel@bilgiedu.net>
2026-05-01 22:12:31 +0300

    Add a simple board layout and a dialog for adding entries

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::{collections::HashMap, path::PathBuf};
 
 use gpui::{
     Action, App, AppContext, ClickEvent, Context, Entity, Focusable, MouseButton, ParentElement,
-    Render, SharedString, Styled, Window, div, prelude::FluentBuilder, px, relative,
+    Render, SharedString, Styled, Window, div, px, relative,
 };
 
 use gpui_component::{
     ActiveTheme, IconName, Root, Theme, ThemeRegistry,
-    breadcrumb::{Breadcrumb, BreadcrumbItem},
-    divider::Divider,
+    button::{Button, ButtonVariants},
+    dialog::{
+        Dialog, DialogAction, DialogClose, DialogDescription, DialogFooter, DialogHeader,
+        DialogTitle,
+    },
     h_flex,
     input::{Input, InputState},
     select::{SearchableVec, Select, SelectDelegate, SelectEvent, SelectState},
@@ -27,6 +30,8 @@ pub struct SidebarElement {
     active_subitem: Option<SubItem>,
     focus_handle: gpui::FocusHandle,
     search_input: Entity<InputState>,
+    dialog_title_input: Entity<InputState>,
+    dialog_description_input: Entity<InputState>,
     theme_select: Entity<SelectState<SearchableVec<SharedString>>>,
 }
 
@@ -40,6 +45,17 @@ impl SidebarElement {
         active_items.insert(Item::Boards, true);
 
         let search_input = cx.new(|cx| InputState::new(window, cx).placeholder("Search..."));
+        let dialog_title_input =
+            cx.new(|cx| InputState::new(window, cx).placeholder("Give your title"));
+
+        let dialog_description_input = cx.new(|cx| {
+            InputState::new(window, cx)
+                .placeholder("Give your description")
+                .multi_line(true)
+                .auto_grow(1, 24)
+                .soft_wrap(true)
+                .searchable(true)
+        });
 
         let registry = ThemeRegistry::global(cx);
         let themes: Vec<SharedString> = registry
@@ -79,6 +95,8 @@ impl SidebarElement {
             active_subitem: None,
             focus_handle: cx.focus_handle(),
             search_input,
+            dialog_title_input,
+            dialog_description_input,
             theme_select,
         }
     }
@@ -120,7 +138,6 @@ impl Item {
             this.last_active_item = item;
             this.active_subitem = None;
             this.focus_handle.focus(window, cx);
-
             cx.notify();
         }
     }
@@ -167,10 +184,14 @@ impl Focusable for SidebarElement {
 impl Render for SidebarElement {
     fn render(
         &mut self,
-        _: &mut gpui::Window,
+        window: &mut gpui::Window,
         cx: &mut gpui::Context<Self>,
     ) -> impl gpui::IntoElement {
         let groups: Vec<Item> = vec![Item::Boards, Item::Notes];
+        let dialog_layer = Root::render_dialog_layer(window, cx);
+
+        let dialog_title_input = self.dialog_title_input.clone();
+        let dialog_description_input = self.dialog_description_input.clone();
 
         h_flex()
             .rounded(cx.theme().radius)
@@ -226,7 +247,7 @@ impl Render for SidebarElement {
                             ),
                     )
                     .child(
-                        SidebarGroup::new("Platform").child(SidebarMenu::new().children(
+                        SidebarGroup::new("Projects").child(SidebarMenu::new().children(
                             groups.iter().enumerate().map(|(_, item)| {
                                 let is_active =
                                     self.last_active_item == *item && self.active_subitem == None;
@@ -264,10 +285,11 @@ impl Render for SidebarElement {
                     ),
             )
             .child(
-                v_flex()
+                h_flex()
                     .size_full()
                     .gap_4()
                     .p_4()
+                    .items_start()
                     .on_mouse_down(
                         MouseButton::Left,
                         cx.listener(|this, _, window, cx| {
@@ -276,34 +298,124 @@ impl Render for SidebarElement {
                         }),
                     )
                     .child(
-                        h_flex()
-                            .items_center()
-                            .gap_3()
-                            .child(Divider::vertical().h_4())
+                        v_flex()
+                            .gap_2()
+                            .w_80()
+                            .p_2()
+                            .bg(cx.theme().secondary)
+                            .text_color(cx.theme().foreground)
+                            .rounded(cx.theme().radius)
+                            .child(div().p_1().font_weight(FontWeight::MEDIUM).child("TODO's"))
                             .child(
-                                Breadcrumb::new()
-                                    .child("Breadcrumb")
-                                    .child(BreadcrumbItem::new("Home").on_click(cx.listener(
-                                        |this, _, window, cx| {
-                                            this.last_active_item = Item::Boards;
-                                            this.focus_handle.focus(window, cx);
-                                            cx.notify();
-                                        },
-                                    )))
-                                    .child(
-                                        BreadcrumbItem::new(self.last_active_item.label())
-                                            .on_click(cx.listener(|this, _, window, cx| {
-                                                this.active_subitem = None;
-                                                this.focus_handle.focus(window, cx);
-                                                cx.notify();
-                                            })),
-                                    )
-                                    .when_some(self.active_subitem, |this, subitem| {
-                                        this.child(BreadcrumbItem::new(subitem.label()))
-                                    }),
+                                div()
+                                    .id("First Item")
+                                    .p_2()
+                                    .bg(cx.theme().primary)
+                                    .text_color(cx.theme().primary_foreground)
+                                    .rounded(cx.theme().radius)
+                                    .hover(|this| {
+                                        this.bg(cx.theme().primary_hover)
+                                            .cursor(CursorStyle::PointingHand)
+                                    })
+                                    .text_sm()
+                                    .w_full()
+                                    .child("First Item On the List"),
+                            )
+                            .child(
+                                div()
+                                    .id("Second Item")
+                                    .p_2()
+                                    .bg(cx.theme().primary)
+                                    .text_color(cx.theme().primary_foreground)
+                                    .text_sm()
+                                    .rounded(cx.theme().radius)
+                                    .hover(|this| {
+                                        this.bg(cx.theme().primary_hover).cursor(CursorStyle::Arrow)
+                                    })
+                                    .w_full()
+                                    .child("Second Item On the List"),
+                            )
+                            .child(
+                                div()
+                                    .id("Third Item")
+                                    .p_2()
+                                    .bg(cx.theme().primary)
+                                    .text_color(cx.theme().primary_foreground)
+                                    .rounded(cx.theme().radius)
+                                    .hover(|this| {
+                                        this.bg(cx.theme().primary_hover).cursor(CursorStyle::Arrow)
+                                    })
+                                    .text_sm()
+                                    .w_full()
+                                    .child("Third Item On the List"),
+                            )
+                            .child(
+                                div().w_full().child(
+                                    Dialog::new(cx)
+                                        .trigger(
+                                            h_flex()
+                                                .id("Add Item")
+                                                .w_full()
+                                                .gap_2()
+                                                .p_1()
+                                                .text_color(cx.theme().secondary_foreground)
+                                                .text_sm()
+                                                .hover(|this| {
+                                                    this.bg(cx.theme().secondary_hover)
+                                                        .text_color(cx.theme().accent_foreground)
+                                                        .cursor(CursorStyle::PointingHand)
+                                                })
+                                                .font_weight(FontWeight::MEDIUM)
+                                                .child(IconName::Plus)
+                                                .child("Add a card"),
+                                        )
+                                        .title("Add a new entry")
+                                        .content({
+                                            move |content, _, _| {
+                                                content
+                                                    .child(
+                                                        DialogHeader::new()
+                                                            .child(
+                                                                DialogTitle::new()
+                                                                    .child("Add a new entry"),
+                                                            )
+                                                            .child(DialogDescription::new().child(
+                                                                "Enter the information needed",
+                                                            )),
+                                                    )
+                                                    .child(
+                                                        v_flex()
+                                                            .gap_2()
+                                                            .child(Input::new(&dialog_title_input))
+                                                            .child(Input::new(
+                                                                &dialog_description_input,
+                                                            )),
+                                                    )
+                                                    .child(
+                                                        DialogFooter::new()
+                                                            .justify_between()
+                                                            .child(
+                                                                DialogClose::new().child(
+                                                                    Button::new("cancel")
+                                                                        .label("Cancel")
+                                                                        .outline(),
+                                                                ),
+                                                            )
+                                                            .child(
+                                                                DialogAction::new().child(
+                                                                    Button::new("confirm")
+                                                                        .primary()
+                                                                        .label("Confirm"),
+                                                                ),
+                                                            ),
+                                                    )
+                                            }
+                                        }),
+                                ),
                             ),
                     ),
             )
+            .children(dialog_layer)
     }
 }
 
@@ -320,7 +432,7 @@ fn main() {
         cx.spawn(async move |cx| {
             cx.open_window(
                 WindowOptions {
-                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    window_bounds: Some(WindowBounds::Maximized(bounds)),
                     ..Default::default()
                 },
                 |window, cx| {
